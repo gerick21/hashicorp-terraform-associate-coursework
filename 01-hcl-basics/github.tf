cf3e8bea15034c265e2133df551b5dadb5a8b877
Gerick Vargas Camacho <gerickcamacho@gmail.com>
2026-05-27 20:44:22 -0600

    Add comments to clarify provider and resource configurations in GitHub Terraform files

--- 01-hcl-basics/github.tf
+++ 01-hcl-basics/github.tf
@@ -1,7 +1,9 @@
+#Declaring the provider and resource. The provider block is where you configure the provider, in this case, the GitHub provider. The resource block is where you define the resources you want to create, in this case, a GitHub repository.
 provider "github" {
   token = var.github_token
 }
 
+#Declaring the resource for the GitHub repository. The name of the resource is "github_repository" and the name of the instance is "production_repo". The name of the repository is "production-repo", and it has a description and is set to private.
 resource "github_repository" "production_repo" {
   name        = "production-repo"
   description = "This is the production repository for the Terraform Associate course."
--- 01-hcl-basics/test.tf
+++ 01-hcl-basics/test.tf
@@ -1,3 +1,4 @@
+#Searate the testing repo, might be a good practices when you have different environments. You can also use workspaces for this, but I wanted to show how to create multiple resources in the same configuration file.
 resource "github_repository" "testing_repo" {
   name        = "testing-repo"
   description = "This is the testing repository for the Terraform Associate course."
