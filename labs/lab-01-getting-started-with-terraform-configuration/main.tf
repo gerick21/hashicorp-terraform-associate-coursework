014294127c120e3335f787b9c4e79b0f3eaaa5ae
Gerick Vargas Camacho <gerickcamacho@gmail.com>
2026-06-14 19:59:07 -0600

    Refactor VPC configuration to use variables for CIDR and environment tags

--- labs/lab-01-getting-started-with-terraform-configuration/main.tf
+++ labs/lab-01-getting-started-with-terraform-configuration/main.tf
@@ -1,12 +1,10 @@
-# Create the primary VPC for workloads
 resource "aws_vpc" "main" {
-  cidr_block           = "192.168.0.0/16" # <-- change IP Address
+  cidr_block           = var.vpc_cidr # <-- update value here
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
     Name        = "terraform-course"
-    Environment = "Learning Terraform"
-    Managed_By  = "Terraform"
+    Environment = var.environment # <-- update value here
   }
 }
