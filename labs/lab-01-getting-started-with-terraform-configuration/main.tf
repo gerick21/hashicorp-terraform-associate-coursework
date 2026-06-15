a953e34776a52a255ab47c7951a589469d429879
Gerick Vargas Camacho <gerickcamacho@gmail.com>
2026-06-14 19:39:43 -0600

    Update VPC CIDR block to 192.168.0.0/16 for lab-01 configuration

--- labs/lab-01-getting-started-with-terraform-configuration/main.tf
+++ labs/lab-01-getting-started-with-terraform-configuration/main.tf
@@ -1,6 +1,6 @@
 # Create the primary VPC for workloads
 resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "192.168.0.0/16" # <-- change IP Address
   enable_dns_hostnames = true
   enable_dns_support   = true
 
