e4de132a43ea2f78b7940b2e0b881e82dcc63ece
Gerick Vargas Camacho <gerickcamacho@gmail.com>
2026-06-28 16:52:00 -0600

    Add subnet module configuration with variables for CIDR, name, and availability zone

--- 05-create-our-own-terraform-modules/terraform/main.tf
+++ 05-create-our-own-terraform-modules/terraform/main.tf
@@ -3,3 +3,14 @@ module "vpc" {
   vpc_cidr = var.cidr_block
   vpc_name = var.vpc_name
 }
+
+
+module "subnet_module" {
+  source            = "./modules/subnet"
+  vpc_id            = module.vpc.vpc_id
+  subnet_cidr       = "10.0.1.0/24"
+  subnet_name       = "my-terraform-subnet"
+  availability_zone = "us-east-1a"
+}
+
+
